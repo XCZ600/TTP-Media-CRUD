f9460740789768474dff555344dceb6177a7570d
XinCi Zheng <xincizheng600@gmail.com>
2021-01-22 09:43:11 -0500

    commit

--- social-media/crud-app/src/components/PostCard.js
+++ social-media/crud-app/src/components/PostCard.js
@@ -1,45 +1,50 @@
-import React from 'react'
-import { Button, Card, Icon, Label, Image } from 'semantic-ui-react'
+import React, { useContext } from 'react'
+import { Button, Card, Container, Icon, Label, Image } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
 
+import { AuthContext } from '../context/auth'
+import LikeButton from './LikeButton'
+
+import DeleteButton from './DeleteButton'
+import MyPopup from '../utils/MyPopup'
+
 function PostCard({ post: { body, createdAt, id, username, likeCount, commentCount, likes } }) {
-    function likePost() {
-        console.log("like post")
-    }
-    function commentOnPost() {
-        console.log("comment on post")
-    }
+
+
+    const { user } = useContext(AuthContext);
 
     return (
-        <Card fluid>
+        <Card fluid >
             <Card.Content>
                 <Image
                     floated='right'
                     size='mini'
-                    src='https://react.semantic-ui.com/images/avatar/large/molly.png'
+                    src='https://icons-for-free.com/iconfiles/png/512/avatar+human+people+profile+user+icon-1320168139431219590.png'
                 />
-                <Card.Header>{username}</Card.Header>
-                <Card.Meta as={Link} to={`/posts${id}`}>{moment(createdAt).fromNow(true)}</Card.Meta>
-                <Card.Description>{body}</Card.Description>
+                <Container as={Link} to={`/posts/${id}`}>
+                    <Card.Header className='header' style={{ color: 'black' }}>{username}</Card.Header>
+                    <Card.Meta >{moment(createdAt).fromNow(true)}</Card.Meta>
+                    <Card.Description style={{ color: 'black' }}>{body}</Card.Description>
+                </Container>
             </Card.Content>
             <Card.Content extra>
-                <Button as='div' labelPosition='right' onClick={likePost}>
-                    <Button color="teal" basic>
-                        <Icon name='heart' />
+
+                <LikeButton user={user} post={{ id, likes, likeCount }} />
+
+                <MyPopup content='Leave a Comment'>
+                    <Button labelPosition='right' as={Link} to={`/posts/${id}`}>
+                        <Button color='blue' basic>
+                            <Icon name='comments' style={{ width: 2 }} />
+                        </Button>
+                        <Label basic color='blue' pointing="left">
+                            {commentCount}
+                        </Label>
                     </Button>
-                    <Label basic color='teal' pointing='left'>
-                        {likeCount}
-                    </Label>
-                </Button>
-                <Button as='div' labelPosition='right' onClick={commentOnPost}>
-                    <Button color="blue" basic>
-                        <Icon name='comments' />
-                    </Button>
-                    <Label basic color='blue' pointing='left'>
-                        {commentCount}
-                    </Label>
-                </Button>
+                </MyPopup>
+
+                {user && user.username === username && <DeleteButton postId={id} />}
+
             </Card.Content>
         </Card>
     )
